e120900d17acfe8a2bad660714828bc16f0592e1
Edson Santos <edson.santos@gmail.com>
2019-09-01 18:30:39 -0300

    Saida em JSON

--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net"
 	"net/http"
 	"time"
 )
+
+//Saida [é a saída de dados para o app]
+type Saida struct {
+	Datahora string `json:"datahora"`
+	Ip       string `json:"ip"`
+}
 
 //IndexPage eh a IndexPage
 func IndexPage(w http.ResponseWriter, r *http.Request) {
@@ -26,7 +33,17 @@ func IndexPage(w http.ResponseWriter, r *http.Request) {
 
 	// let's get the request HTTP header "X-Forwarded-For (XFF)"
 	// if the value returned is not null, then this is the real IP address of the user.
-	fmt.Fprintf(w, "X-Forwarded-For :"+r.Header.Get("X-FORWARDED-FOR"))
+	fmt.Fprintf(w, "X-Forwarded-For :"+r.Header.Get("X-FORWARDED-FOR")+"\n\n")
+
+	dadossaida := Saida{t, ip}
+
+	j, err := json.Marshal(dadossaida)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Fprintln(w, "dados de saída: ", string(j))
 }
 
 func dataAtual() string {
@@ -40,6 +57,7 @@ func dataAtual() string {
 }
 
 func main() {
+
 	fmt.Println("Iniciando servidor Web")
 
 	http.HandleFunc("/", IndexPage)
