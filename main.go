411312c986791926aca65966babb9514063e69fa
Edson Santos <edson.santos@gmail.com>
2019-09-01 21:59:34 -0300

    Saida em JSON

--- main.go
+++ main.go
@@ -20,20 +20,20 @@ func IndexPage(w http.ResponseWriter, r *http.Request) {
 	//Exibe data
 	t := dataAtual()
 
-	fmt.Fprintf(w, t+"\n\n")
+	//fmt.Fprintf(w, t+"\n\n")
 
 	// get client ip address
 	ip, _, _ := net.SplitHostPort(r.RemoteAddr)
 
 	// print out the ip address
-	fmt.Fprintf(w, ip+"\n\n")
+	//fmt.Fprintf(w, ip+"\n\n")
 
 	// sometimes, the user acccess the web server via a proxy or load balancer.
 	// The above IP address will be the IP address of the proxy or load balancer and not the user's machine.
 
 	// let's get the request HTTP header "X-Forwarded-For (XFF)"
 	// if the value returned is not null, then this is the real IP address of the user.
-	fmt.Fprintf(w, "X-Forwarded-For :"+r.Header.Get("X-FORWARDED-FOR")+"\n\n")
+	//fmt.Fprintf(w, "X-Forwarded-For :"+r.Header.Get("X-FORWARDED-FOR")+"\n\n")
 
 	dadossaida := Saida{t, ip}
 
@@ -43,7 +43,7 @@ func IndexPage(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(err)
 	}
 
-	fmt.Fprintln(w, "dados de saída: ", string(j))
+	fmt.Fprintln(w, string(j))
 }
 
 func dataAtual() string {
